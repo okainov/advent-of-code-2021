5ed0c3c0c8511945420b447b2a5e9c3a2c4f7a14
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-01 06:19:57 +0100

    day_2: add part 2, refactor

--- python/day_1.py
+++ python/day_1.py
@@ -1,16 +1,28 @@
 import os
 
+
 if __name__ == '__main__':
     result = 0
-    prev = None
+    prev = 0
+    stack = []
+    # 1 for first part, 3 for second part
+    window_length = 3
     with open(os.path.join('..', 'day_1_input.txt'), 'r') as f:
         for line in f:
             current = int(line)
-            if prev and current > prev:
+            prev_sum = sum(stack)
+            stack.append(current)
+
+            if len(stack) > window_length:
+                stack.pop(0)
+            else:
+                continue
+
+            current_sum = sum(stack)
+            if prev_sum < current_sum:
                 result += 1
-            prev = current
 
     print(result)
 
     # First part answer:  1226
-    # Second part answer: 4773483
+    # Second part answer: 1252
