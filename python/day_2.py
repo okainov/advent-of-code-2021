f4cc9825a0c142acd0b5cf374967b5d422854c4e
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-02 06:08:09 +0100

    day_2: add part 2

--- python/day_2.py
+++ python/day_2.py
@@ -1,24 +1,24 @@
 import os
 
-
 if __name__ == '__main__':
     depth = 0
     horizontal = 0
+    aim = 0
     with open(os.path.join('..', 'day_2_input.txt'), 'r') as f:
         for line in f:
             command, amount = line.split()
             amount = int(amount)
             if command == 'forward':
                 horizontal += amount
-            elif command == 'down':
-                depth += amount
-            elif command == 'up':
-                depth -= amount
+                depth += aim * amount
                 if depth < 0:
                     depth = 0
+            elif command == 'down':
+                aim += amount
+            elif command == 'up':
+                aim -= amount
 
-    #print(depth * horizontal)
-    print(depth*horizontal)
+    print(depth * horizontal)
 
     # First part answer:  1728414
-    # Second part answer: 1252
+    # Second part answer: 1765720035
