5ad62f60c113fb26d48600653780bb5bd50ae692
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-03 06:26:44 +0100

    day_3: add shitty part 2

--- python/day_3.py
+++ python/day_3.py
@@ -1,4 +1,44 @@
 import os
+from copy import copy
+
+
+def get_most_common_in_position(inputs, position, default_value='1'):
+    n_zeros = 0
+    n_ones = 0
+    for line in inputs:
+        if line[position] == '0':
+            n_zeros += 1
+        else:
+            n_ones += 1
+    if n_zeros == n_ones:
+        return default_value
+    else:
+        return '1' if n_zeros < n_ones else '0'
+
+
+def get_least_common_in_position(inputs, position, default_value='0'):
+    n_zeros = 0
+    n_ones = 0
+    for line in inputs:
+        if line[position] == '0':
+            n_zeros += 1
+        else:
+            n_ones += 1
+    if n_zeros == n_ones:
+        return default_value
+    else:
+        return '0' if n_zeros < n_ones else '1'
+
+
+def filter_inputs(inputs, filter_f):
+    for position in range(0, len(inputs[0])):
+        most_common = filter_f(inputs, position)
+        inputs = [x for x in inputs if x[position] == most_common]
+        if len(inputs) == 1:
+            break
+
+    return inputs[0]
+
 
 if __name__ == '__main__':
     depth = 0
@@ -6,31 +46,15 @@ if __name__ == '__main__':
     aim = 0
     counter = {
     }
+    inputs = []
     with open(os.path.join('..', 'day_3_input.txt'), 'r') as f:
         for line in f:
-            for i, bit in enumerate(line.strip()):
-                if i not in counter:
-                    counter[i] = {'0': 0, '1': 0}
-                counter[i][bit] += 1
+            inputs.append(line.strip())
 
-    gamma  = ''
-    epsilon = ''
-    for i in range(50):
-        if i not in counter:
-            continue
-        if counter[i]['0'] > counter[i]['1']:
-            gamma += '0'
-            epsilon += '1'
-        else:
-            gamma += '1'
-            epsilon += '0'
+    oxygen = int(filter_inputs(copy(inputs), get_most_common_in_position), 2)
+    co2 = int(filter_inputs(copy(inputs), get_least_common_in_position), 2)
 
-
-    print('Gamma binary ' + gamma)
-    print('epsilon binary ' + epsilon)
-    gamma = int(gamma, 2)
-    epsilon = int(epsilon, 2)
-    print(epsilon*gamma)
+    print(oxygen * co2)
 
     # First part answer:  1458194
-    # Second part answer: 1765720035
+    # Second part answer: 2829354
