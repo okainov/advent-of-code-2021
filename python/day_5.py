2c91dea282d4be8fa0dff872d2c42805936a04a7
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-05 06:23:26 +0100

    day_5: refactor to unify code

--- python/day_5.py
+++ python/day_5.py
@@ -1,41 +1,5 @@
-import operator
 import os
 
-
-def mark_number_in_board(board, number):
-    sum_unmarked = 0
-    marked = False
-    for i in board:
-        for j in board[i]:
-            current_number = list(board[i][j].keys())[0]
-            if number == current_number:
-                board[i][j][number] = True
-                marked = True
-            if not board[i][j][current_number]:
-                sum_unmarked += int(current_number)
-    return marked, sum_unmarked
-
-
-def is_winning(board):
-    for i in range(len(board)):
-        # Check column i
-        column_all_trues = True
-        for line in board:
-            current_number = list(board[line][i].keys())[0]
-            if not board[line][i][current_number]:
-                column_all_trues = False
-                break
-        if column_all_trues:
-            return True
-
-        # Check line i
-        row_all_trues = True
-        for q in board[i]:
-            current_number = list(board[i][q].keys())[0]
-            if not board[i][q][current_number]:
-                row_all_trues = False
-        if row_all_trues:
-            return True
 
 def sum_pairs(pair_1, pair_2):
     return pair_1[0] + pair_2[0], pair_1[1] + pair_2[1]
@@ -57,55 +21,51 @@ if __name__ == '__main__':
                     coords.append(number)
             x1, y1, x2, y2 = coords
 
-
+            x_inc = 0
+            y_inc = 0
 
             if x1 == x2:
                 # Horizontal
-                # Swap just in case so y2 is always bigger
                 if y2 < y1:
-                    y1, y2 = y2, y1
-                if x1 not in field:
-                    field[x1] = {}
-                for y in range(y1, y2+1):
-                    if y not in field[x1]:
-                        field[x1][y] = 0
-                    field[x1][y] += 1
+                    y_inc = -1
+                else:
+                    y_inc = 1
             elif y1 == y2:
                 # Vertical
-                # Swap just in case so x2 is always bigger
                 if x2 < x1:
-                    x1, x2 = x2, x1
-                for x in range(x1, x2+1):
-                    if x not in field:
-                        field[x] = {}
-                    if y1 not in field[x]:
-                        field[x][y1] = 0
-                    field[x][y1] += 1
+                    x_inc = -1
+                else:
+                    x_inc = 1
             else:
                 # Should be diagonal
+                # Uncomment to get part 1
+                # continue
                 x_inc = 1
                 y_inc = 1
                 if x2 < x1:
                     x_inc = -1
                 if y2 < y1:
                     y_inc = -1
-                increment = (x_inc, y_inc)
+            increment = (x_inc, y_inc)
 
-                current_point = (x1, y1)
+            current_point = (x1, y1)
 
-                while True:
-                    x, y = current_point
-                    if x not in field:
-                        field[x] = {}
-                    if y not in field[x]:
-                        field[x][y] = 0
-                    field[x][y] += 1
+            if increment == (0, 0):
+                # Not a real case, but just to handle part1/2 switch
+                continue
 
-                    if current_point == (x2, y2):
-                        break
+            while True:
+                x, y = current_point
+                if x not in field:
+                    field[x] = {}
+                if y not in field[x]:
+                    field[x][y] = 0
+                field[x][y] += 1
 
-                    current_point = sum_pairs(current_point, increment)
+                if current_point == (x2, y2):
+                    break
 
+                current_point = sum_pairs(current_point, increment)
 
     overlaps = 0
     for x in field:
