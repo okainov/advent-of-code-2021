e81eccefe0d8a5b1c91781d38bb5293e452b5e5d
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-07 06:09:55 +0100

    day_7: add part 2
    Naive approach was too slow, had to add
    memoizatioin

--- python/day_7.py
+++ python/day_7.py
@@ -1,5 +1,22 @@
 import os
 
+dist_map = {}
+
+
+def calculate_consumption(old_pos, new_pos):
+    distance = abs(old_pos - new_pos)
+    result = 0
+    cost = 1
+    if distance in dist_map:
+        return dist_map[distance]
+    for i in range(distance):
+        result += cost
+        cost += 1
+    dist_map[distance] = result
+    # Part 1
+    # return distance
+    return result
+
 
 if __name__ == '__main__':
     first = True
@@ -12,20 +29,18 @@ if __name__ == '__main__':
             line = line.strip()
             positions = [int(x) for x in line.split(',')]
 
-
-    #just bruteforce
+    # just bruteforce
 
     min_fuel = 999999999999999
 
     for i in range(0, max(positions) + 1):
         current_fuel = 0
         for position in positions:
-            current_fuel += abs(position-i)
+            current_fuel += calculate_consumption(i, position)
         if current_fuel < min_fuel:
             min_fuel = current_fuel
 
-
     print(min_fuel)
 
-    # First part answer:  345387
-    # Second part answer: 1574445493136
+    # First part answer:  337488
+    # Second part answer: 89647695
