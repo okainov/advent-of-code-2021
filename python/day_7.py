87ae69603f39d004d9d982066e3f373ad2f68404
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-07 06:15:37 +0100

    day_7: simplify formulas, count both parts

--- python/day_7.py
+++ python/day_7.py
@@ -1,21 +1,16 @@
 import os
 
-dist_map = {}
 
-
-def calculate_consumption(old_pos, new_pos):
+def calculate_consumption_2(old_pos, new_pos):
     distance = abs(old_pos - new_pos)
-    result = 0
-    cost = 1
-    if distance in dist_map:
-        return dist_map[distance]
-    for i in range(distance):
-        result += cost
-        cost += 1
-    dist_map[distance] = result
-    # Part 1
-    # return distance
-    return result
+
+    return int(distance * (distance + 1) / 2)
+
+
+def calculate_consumption_1(old_pos, new_pos):
+    distance = abs(old_pos - new_pos)
+
+    return distance
 
 
 if __name__ == '__main__':
@@ -29,18 +24,24 @@ if __name__ == '__main__':
             line = line.strip()
             positions = [int(x) for x in line.split(',')]
 
-    # just bruteforce
+    # Just bruteforce seems to be fast enough
 
-    min_fuel = 999999999999999
+    min_fuel_1 = 999999999999999
+    min_fuel_2 = 999999999999999
 
     for i in range(0, max(positions) + 1):
-        current_fuel = 0
+        current_fuel_1 = 0
+        current_fuel_2 = 0
         for position in positions:
-            current_fuel += calculate_consumption(i, position)
-        if current_fuel < min_fuel:
-            min_fuel = current_fuel
+            current_fuel_1 += calculate_consumption_1(i, position)
+            current_fuel_2 += calculate_consumption_2(i, position)
+        if current_fuel_1 < min_fuel_1:
+            min_fuel_1 = current_fuel_1
+        if current_fuel_2 < min_fuel_2:
+            min_fuel_2 = current_fuel_2
 
-    print(min_fuel)
+    print(f'Part 1: {min_fuel_1}')
+    print(f'Part 2: {min_fuel_2}')
 
     # First part answer:  337488
     # Second part answer: 89647695
