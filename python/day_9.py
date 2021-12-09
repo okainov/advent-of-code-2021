08139bcec20479800bda4306f69f81b5928f052e
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-09 06:40:52 +0100

    chore: move common func to utils

--- python/day_9.py
+++ python/day_9.py
@@ -1,9 +1,6 @@
 import os
 
-
-def sum_pairs(pair_1, pair_2):
-    return pair_1[0] + pair_2[0], pair_1[1] + pair_2[1]
-
+from python.utils import sum_pairs
 
 if __name__ == '__main__':
     matrix = {}
