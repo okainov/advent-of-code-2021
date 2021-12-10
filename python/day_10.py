825923bf2a242a746843f3a3c5e769da05c0fc51
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-10 06:16:14 +0100

    day_10: add part 2

--- python/day_10.py
+++ python/day_10.py
@@ -1,8 +1,6 @@
 import os
 
 if __name__ == '__main__':
-    matrix = {}
-    i = 0
     mapping = {
         '{': '}',
         '(': ')',
@@ -15,21 +13,40 @@ if __name__ == '__main__':
         ']': 57,
         '>': 25137
     }
-    score = 0
+    complete_scores = {
+        '{': 3,
+        '(': 1,
+        '[': 2,
+        '<': 4
+    }
+    corrupt_score = 0
+    line_scores = []
     with open(os.path.join('..', 'day_10_input.txt'), 'r') as f:
         for line in f:
             line = line.strip()
             stack = []
+            line_score = 0
             for c in line:
                 if c in ['(', '{', '[', '<']:
                     stack.append(c)
                 elif c == mapping[stack[-1]]:
                     stack.pop()
                 else:
-                    score += scores[c]
+                    corrupt_score += scores[c]
                     break
+            else:
+                # Line is incomplete
+                for leftover in reversed(stack):
+                    line_score *= 5
+                    line_score += complete_scores[leftover]
+                line_scores.append(line_score)
 
-    print(f'Part 1: {score}')
+    # Strip down the scores to find a median
+    while len(line_scores) != 1:
+        line_scores.remove(max(line_scores))
+        line_scores.remove(min(line_scores))
+    print(f'Part 1: {corrupt_score}')
+    print(f'Part 2: {line_scores[0]}')
 
     # First part answer:  315693
-    # Second part answer: 1558722
+    # Second part answer: 1870887234
