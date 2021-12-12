a3c202bb8c4f6bf910fc0ab1a9c5797f946e8de6
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-12 06:52:54 +0100

    day_12: add slow part 2

--- python/day_12.py
+++ python/day_12.py
@@ -1,11 +1,7 @@
 import os
 
 if __name__ == '__main__':
-    matrix = {}
-    i = 0
-    edges = []
     vertexes = {}
-    small_caves = set()
     with open(os.path.join('..', 'day_12_input.txt'), 'r') as f:
         for line in f:
             line = line.strip()
@@ -16,17 +12,9 @@ if __name__ == '__main__':
                 vertexes[end] = []
             vertexes[start].append(end)
             vertexes[end].append(start)
-            edges.append((start, end))
-            edges.append((end, start))
-            if start == start.lower() and len(start) == 1:
-                small_caves.add(start)
-            if end == end.lower() and len(end) == 1:
-                small_caves.add(end)
 
     stack = [['start']]
     number = 0
-    visited = {}
-
     while stack:
         current_route = stack.pop()
         current = current_route[-1]
@@ -42,14 +30,27 @@ if __name__ == '__main__':
             for v in suggested_route:
                 if not v.islower():
                     continue
-                if v in counter:
-                    bad_route = True
-                counter[v] = 1
-            if bad_route:
-                continue
-            stack.append(current_route + [neightbor])
+                if v not in counter:
+                    counter[v] = 0
 
-    print(f'Part 1: {number}')
+                if counter[v] > 0 and v in ['start', 'end']:
+                    # Don't visit start/end again
+                    bad_route = True
+                    break
+                elif counter[v] > 1:
+                    # Don't visit anything more than trice
+                    bad_route = True
+                    break
+                counter[v] += 1
+            how_many_gonna_go_twice = len([n for n in counter.values() if n > 1])
+            solving_part_1 = how_many_gonna_go_twice > 0
+            solving_part_2 = how_many_gonna_go_twice > 1
+            # Replace condition for corresponding part
+            if bad_route or solving_part_2:
+                continue
+            stack.append(suggested_route)
+
+    print(f'Result: {number}')
 
     # First part answer: 3000
-    # Second part answer:
+    # Second part answer: 74222
