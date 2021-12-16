97b513a9f89e7d45a09a8aed177e110c38208b4b
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-16 09:28:45 +0100

    day_16: refactor, add part 2

--- python/day_16.py
+++ python/day_16.py
@@ -1,62 +1,152 @@
-import heapq
 import os
 
-from utils import sum_pairs
+
+class Packet:
+    def __init__(self, packet_version, packet_type, operation, subpackets, position):
+        self.version = packet_version
+        self.type = packet_type
+        self.operation = operation
+        self.subpackets = subpackets
+        self.size = position
+
+    def __repr__(self):
+        if isinstance(self.operation, int):
+            return 'Number ' + str(self.operation)
+        return f'{self.operation}: Version {self.version}, type {self.type}, size {self.size}, subpackets {len(self.subpackets)}'
+
+    def calculate(self):
+        if isinstance(self.operation, int):
+            return self.operation
+
+        if self.operation == 'sum':
+            return sum([x.calculate() for x in self.subpackets])
+        elif self.operation == 'product':
+            res = 1
+            for p in self.subpackets:
+                res *= p.calculate()
+            return res
+        elif self.operation == 'min':
+            return min([x.calculate() for x in self.subpackets])
+        elif self.operation == 'max':
+            return max([x.calculate() for x in self.subpackets])
+        elif self.operation == 'lt':
+            return 1 if self.subpackets[0].calculate() < self.subpackets[1].calculate() else 0
+        elif self.operation == 'gt':
+            return 1 if self.subpackets[0].calculate() > self.subpackets[1].calculate() else 0
+        elif self.operation == 'eq':
+            return 1 if self.subpackets[0].calculate() == self.subpackets[1].calculate() else 0
+
+    def sum_versions(self):
+        return self.version + sum([x.sum_versions() for x in self.subpackets])
+
+    def get_total_size(self):
+        return self.size + sum([x.get_total_size() for x in self.subpackets])
 
 
-def print_path(costs, x, y):
-    prev_cost = costs[x, y]
-    while x > 0 or y > 0:
-        if x == 0 or (x - 1, y) not in costs:
-            y -= 1
-        elif y == 0 or (x, y - 1) not in costs:
-            x -= 1
-        else:
-            if costs[x - 1, y] > costs[x, y - 1]:
-                y -= 1
-            else:
-                x -= 1
-        print(f'From ({x}, {y}) walk via {prev_cost - costs[x, y]}')
-        prev_cost = costs[x, y]
-
-
-def make_costs(matrix, to_pos, size, tor_size):
-    queue = []
-    heapq.heappush(queue, (0, (0, 0)))
-    costs = {(0, 0): 0}
-    visited = set()
-    while queue:
-        _, current_position = heapq.heappop(queue)
-        if current_position in visited:
-            continue
-        visited.add(current_position)
-        current_length = costs[current_position]
-        if to_pos in visited:
+def read_packet(bits, max_packets=0):
+    position = 0
+    total_versions = 0
+    packets = []
+    while position < len(bits):
+        if max_packets and len(packets) >= max_packets:
             break
+        packet_version_bin = bits[position:position + 3]
+        position += 3
+        packet_type_bin = bits[position:position + 3]
+        position += 3
+        # if ('1' not in packet_version_bin and '1' not in packet_type_bin) or not packet_type_bin:
+        #     break
+        # print('-' * 40)
+        try:
+            packet_version = int(packet_version_bin, 2)
+            packet_type = int(packet_type_bin, 2)
+        except:
+            break
+        # if packet_version == 0 and packet_type == 0:
+        #     break
+        total_versions += packet_version
+        # print(f'Packet Version {packet_version_bin} == {packet_version}')
+        # print(f'Packet Type {packet_type_bin} == {packet_type}')
 
-        for neightbor in (sum_pairs(current_position, (1, 0)),
-                          sum_pairs(current_position, (0, 1)),
-                          sum_pairs(current_position, (-1, 0)),
-                          sum_pairs(current_position, (0, -1))
-                          ):
-            if neightbor[0] >= size or neightbor[0] < 0 or \
-                    neightbor[1] < 0 or neightbor[1] >= size or neightbor in visited:
-                continue
-            adds = neightbor[0] // tor_size
-            real_x = neightbor[0] % tor_size
-            adds += neightbor[1] // tor_size
-            real_y = neightbor[1] % tor_size
-            neightbor_val = (matrix[real_x][real_y] + adds - 1) % 9 + 1
-            costs[neightbor] = min(current_length + neightbor_val, costs.get(neightbor, 99999999999999))
-            heapq.heappush(queue, (costs[neightbor], neightbor))
-    return costs
+        if packet_type == 4:
+            packet_size = 6
+            number_bits = ''
+            while bits[position] == '1':
+                # Starts with 1, continue reading
+                number_part = bits[position + 1:position + 5]
+                number_bits += ''.join(number_part)
+                position += 5
+                packet_size += 5
+            else:
+                # Last group
+                number_part = bits[position + 1:position + 5]
+                number_bits += ''.join(number_part)
+                position += 5
+                packet_size += 5
+            encoded_number = int(number_bits, 2)
+            packets.append(Packet(packet_version, packet_type, encoded_number, [], packet_size))
+        else:
+            packet_size = 6
+            if packet_type == 0:
+                op = 'sum'
+            elif packet_type == 1:
+                op = 'product'
+            elif packet_type == 2:
+                op = 'min'
+            elif packet_type == 3:
+                op = 'max'
+            elif packet_type == 5:
+                op = 'gt'
+            elif packet_type == 6:
+                op = 'lt'
+            elif packet_type == 7:
+                op = 'eq'
+            else:
+                raise Exception('Unknown type')
+
+            try:
+                mode_bit = bits[position]
+            except:
+                break
+            position += 1
+            packet_size += 1
+            if mode_bit == '0':
+                # next 15 bits are total length in bits
+                total_length_bits = bits[position:position + 15]
+                try:
+                    total_length = int(total_length_bits, 2)
+                except:
+                    break
+                position += 15
+                packet_size += 15
+                # print(f'Package {packet_type}, length: {total_length}')
+                real_subpackets = read_packet(bits[position:position + total_length])
+                position += total_length
+            else:
+                # Next 11 bits are number of subpackets
+                subpackets_bits = bits[position:position + 11]
+                subpackets = int(subpackets_bits, 2)
+                position += 11
+                packet_size += 11
+                # print(f'Package {packet_type}, subpackets: {subpackets}')
+                real_subpackets = []
+                while len(real_subpackets) != subpackets:
+                    packets_t = read_packet(bits[position:], subpackets)
+                    for pack in packets_t:
+                        position += pack.get_total_size()
+                        real_subpackets.append(pack)
+                        if len(real_subpackets) == subpackets:
+                            break
+
+            packets.append(Packet(packet_version, packet_type, op, real_subpackets, packet_size))
+    return packets
 
 
 if __name__ == '__main__':
     matrix = {}
     row = 0
     size_cave = 0
-    bits = []
+    bits = ''
     mapping = {
         '0': '0000',
         '1': '0001',
@@ -79,72 +169,14 @@ if __name__ == '__main__':
         for line in f:
             line = line.strip()
             for i, c in enumerate(line):
-                bit_str = mapping[c]
-                for bit in bit_str:
-                    bits.append(bit)
+                bits += mapping[c]
 
-    position = 0
+    packets = read_packet(bits)
+    pack = packets[0]
 
-    total_versions = 0
-    while position < len(bits):
-        packet_version_bin = ''.join(bits[position:position + 3])
-        position += 3
-        packet_type_bin = ''.join(bits[position:position + 3])
-        position += 3
-        # if ('1' not in packet_version_bin and '1' not in packet_type_bin) or not packet_type_bin:
-        #     break
-        print('-' * 40)
-        try:
-            packet_version = int(packet_version_bin, 2)
-            packet_type = int(packet_type_bin, 2)
-        except:
-            break
-        # if packet_version == 0 and packet_type == 0:
-        #     break
-        total_versions += packet_version
-        print(f'Packet Version {packet_version_bin} == {packet_version}')
-        print(f'Packet Type {packet_type_bin} == {packet_type}')
-
-        if packet_type == 4:
-            number_bits = ''
-            while bits[position] == '1':
-                # Starts with 1, continue reading
-                number_part = bits[position + 1:position + 5]
-                number_bits += ''.join(number_part)
-                position += 5
-            else:
-                # Last group
-                number_part = bits[position + 1:position + 5]
-                number_bits += ''.join(number_part)
-                position += 5
-            encoded_number = int(number_bits, 2)
-            print(f'Package 4, number: {encoded_number}')
-        else:
-            try:
-                mode_bit = bits[position]
-            except:
-                break
-            position += 1
-            if mode_bit == '0':
-                # next 15 bits are total length in bits
-                total_length_bits = ''.join(bits[position:position + 15])
-                try:
-                    total_length = int(total_length_bits, 2)
-                except:
-                    break
-                position += 15
-                print(f'Package {packet_type}, length: {total_length}')
-                continue
-            else:
-                # Next 11 are nimber of subpoackerts
-                subpackets_bits = ''.join(bits[position:position + 11])
-                subpackets = int(subpackets_bits, 2)
-                position += 11
-                print(f'Package {packet_type}, subpackets: {subpackets}')
-                continue
-
-    print(f'Part 1: {total_versions}')
-    # print(f'Part 2: {costs_b[real_size - 1, real_size - 1]}')
+    print(pack)
+    print(f'Part 1: {pack.sum_versions()}')
+    print(f'Part 2: {pack.calculate()}')
 
 # First part answer:  965
-# Second part answer: 3063
+# Second part answer: 116672213160
