ed625bba82d5b76fa79b1bb5dd33396fa5cdcece
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-18 08:50:53 +0100

    chore: temp state before refactoring explosion

--- python/day_18.py
+++ python/day_18.py
@@ -26,10 +26,12 @@ def get_magnitude(fishes):
 class Node:
     @staticmethod
     def build_node(l, parent=None, level=0):
+        if isinstance(l, Node):
+            return l
         if isinstance(l, int):
-            return Node(value=l, parent=parent)
+            return Node(value=l, parent=parent, level=level)
         if len(l) == 1:
-            return Node.build_node(l[0])
+            return Node.build_node(l[0], level=level)
         root = Node(parent=parent, level=level)
         left_node = Node.build_node(l[0], parent=root, level=level + 1)
         right_node = Node.build_node(l[1], parent=root, level=level + 1)
@@ -44,6 +46,23 @@ class Node:
         self.parent = parent
         self.level = level
 
+    def get_level(self):
+        level = 0
+        current = self.parent
+        while current is not None:
+            level += 1
+            current = current.parent
+        return level
+
+    @staticmethod
+    def merge_nodes(node1, node2):
+        root = Node(parent=None, level=0)
+        node1.parent = root
+        root.left = node1
+        node2.parent = root
+        root.right = node2
+        return root
+
     def __repr__(self):
         if self.value is not None:
             return f'{self.value}'
@@ -52,11 +71,26 @@ class Node:
 
 
 def find_left_number(node):
+    parent = node.parent
+    # Go up
+    while parent and parent.left == node:
+        parent = parent.parent
+        node = node.parent
+
     # Node is pre-leaf node with both left-right as numbers, pair to explode
-    if node and node.parent and node.parent.left and node.parent.left.value is not None:
-        return node.parent
+    if parent and parent.left and parent.left.value is not None:
+        # Just a number
+        return parent
+    elif parent and parent.left and parent.left.right and parent.left.right.value is not None:
+        return parent.left
     else:
         return None
+
+    # # Node is pre-leaf node with both left-right as numbers, pair to explode
+    # if node and node.parent and node.parent.left and node.parent.left.value is not None:
+    #     return node.parent
+    # else:
+    #     return None
 
 
 def find_right_number(node):
@@ -78,22 +112,29 @@ def find_right_number(node):
 def traverse_tree(node):
     if not node:
         return
-    if node.level == 4 and node.left and isinstance(node.left.value, int) and node.right and isinstance(
+    if node.get_level() == 4 and node.left and isinstance(node.left.value, int) and node.right and isinstance(
             node.right.value, int):
         # Explode
         print(f'Exploding {node}')
         left_to_explode = find_left_number(node)
         if left_to_explode:
-            print(f'Left explosion: {left_to_explode.left.value}+={node.left.value}')
-            left_to_explode.left.value += node.left.value
+            if left_to_explode.left.value is not None:
+                # print(f'Left explosion(v): {left_to_explode.left.value}+={node.left.value}')
+                left_to_explode.left.value += node.left.value
+            else:
+                # print(f'Left explosion: {left_to_explode.right.value}+={node.left.value}')
+                left_to_explode.right.value += node.left.value
+
+            # print(f'Left explosion: {left_to_explode.left.value}+={node.left.value}')
+            # left_to_explode.left.value += node.left.value
         right_to_explode = find_right_number(node)
         if right_to_explode:
-            if right_to_explode.right.value is not None:
-                print(f'Right explosion(v): {right_to_explode.right.value}+={node.right.value}')
-                right_to_explode.right.value += node.right.value
-            else:
-                print(f'Right explosion: {right_to_explode.left.value}+={node.right.value}')
+            if right_to_explode.left.value is not None:
+                # print(f'Right explosion: {right_to_explode.left.value}+={node.right.value}')
                 right_to_explode.left.value += node.right.value
+            else:
+                # print(f'Right explosion(v): {right_to_explode.right.value}+={node.right.value}')
+                right_to_explode.right.value += node.right.value
         # Replace pair
         node.left = None
         node.right = None
@@ -106,6 +147,27 @@ def traverse_tree(node):
         action_happened = traverse_tree(node.left)
     if not action_happened:
         action_happened = traverse_tree(node.right)
+    return action_happened
+
+
+def traverse_tree_for_split(node):
+    if not node:
+        return
+    if node.value is not None and node.value >= 10:
+        # Split
+        print(f'Splitting {node}')
+
+        node.left = Node(parent=node, value=node.value // 2)
+        node.right = Node(parent=node, value=node.value // 2 + node.value % 2)
+        node.value = None
+        return True
+    # if node.value is not None:
+    #     print(node.value)
+    action_happened = False
+    if not action_happened:
+        action_happened = traverse_tree_for_split(node.left)
+    if not action_happened:
+        action_happened = traverse_tree_for_split(node.right)
     return action_happened
 
 
@@ -131,24 +193,43 @@ def traverse(item, level=0):
         yield item
 
 
+def reduce_fish(fish):
+    action_happened = True
+    while action_happened:
+        print('----Before   ' + str(fish))
+        explode_happened = traverse_tree(fish)
+        split_happened = False
+        if not explode_happened:
+            split_happened = traverse_tree_for_split(fish)
+        action_happened = explode_happened or split_happened
+        print('----After   ' + str(fish))
+    print(fish)
+
+
 if __name__ == '__main__':
     fishes = []
     with open(os.path.join('..', 'day_18_input.txt'), 'r') as f:
         for line in f:
             line = line.strip()
-            fishes.append(json.loads(line))
+            fishes.append(Node.build_node(json.loads(line)))
 
     while len(fishes) > 1:
-        fishes = [[fishes[0], fishes[1]]] + fishes[2:]
+        print('='*40)
+        print('Adding')
+        print(' +' + str(fishes[0]))
+        print(' +' + str(fishes[1]))
+        new_fish = Node.merge_nodes(fishes[0], fishes[1])
+        print(' =' + str(new_fish))
+        print('Reducing')
+        reduce_fish(new_fish)
+        print(' =' + str(new_fish))
+        fishes = [new_fish] + fishes[2:]
 
     print(str(fishes))
-    print('-'*20)
+    print('-' * 20)
     qqq = Node.build_node(fishes)
-    print(qqq)
-    print('-'*20)
-    traverse_tree(qqq)
-    print(qqq)
-    #print(fishes)
+    reduce_fish(qqq)
+    # print(fishes)
 
     print(f'Part 1: {get_magnitude(fishes[0])}')
     # print(f'Part 2: {good_speeds}')
