688fc181d8f8a51f9004cc5f2585584acfdc8147
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-19 10:16:45 +0100

    day 19: refactor a bit, but still slow

--- python/day_19.py
+++ python/day_19.py
@@ -1,6 +1,136 @@
 import os
 from copy import copy
 
+# http://www.euclideanspace.com/maths/algebra/matrix/transforms/examples/index.htm
+ROTATION_MATRIXES = [
+    {
+        0: (1, 0, 0),
+        1: (0, 1, 0),
+        2: (0, 0, 1),
+    },
+    {
+        0: (1, 0, 0),
+        1: (0, 0, -1),
+        2: (0, 1, 0),
+    },
+    {
+        0: (1, 0, 0),
+        1: (0, -1, 0),
+        2: (0, 0, -1),
+    },
+    {
+        0: (1, 0, 0),
+        1: (0, 0, 1),
+        2: (0, -1, 0),
+    },
+    # 4 above
+
+    {
+        0: (0, -1, 0),
+        1: (1, 0, 0),
+        2: (0, 0, 1),
+    },
+    {
+        0: (0, 0, 1),
+        1: (1, 0, 0),
+        2: (0, 1, 0),
+    },
+    {
+        0: (0, 1, 0),
+        1: (1, 0, 0),
+        2: (0, 0, -1),
+    },
+    {
+        0: (0, 0, -1),
+        1: (1, 0, 0),
+        2: (0, -1, 0),
+    },
+    # 4 above
+    {
+        0: (-1, 0, 0),
+        1: (0, -1, 0),
+        2: (0, 0, 1),
+    },
+    {
+        0: (-1, 0, 0),
+        1: (0, 0, -1),
+        2: (0, -1, 0),
+    },
+    {
+        0: (-1, 0, 0),
+        1: (0, 1, 0),
+        2: (0, 0, -1),
+    },
+    {
+        0: (-1, 0, 0),
+        1: (0, 0, 1),
+        2: (0, 1, 0),
+    },
+    # 4 above
+    {
+        0: (0, 1, 0),
+        1: (-1, 0, 0),
+        2: (0, 0, 1),
+    },
+    {
+        0: (0, 0, 1),
+        1: (-1, 0, 0),
+        2: (0, -1, 0),
+    },
+    {
+        0: (0, -1, 0),
+        1: (-1, 0, 0),
+        2: (0, 0, -1),
+    },
+    {
+        0: (0, 0, -1),
+        1: (-1, 0, 0),
+        2: (0, 1, 0),
+    },
+    # 4 above
+    {
+        0: (0, 0, -1),
+        1: (0, -1, 0),
+        2: (-1, 0, 0),
+    },
+    {
+        0: (0, 0, 1),
+        1: (0, 1, 0),
+        2: (-1, 0, 0),
+    },
+    {
+        0: (0, 1, 0),
+        1: (0, 0, -1),
+        2: (-1, 0, 0),
+    },
+    {
+        0: (0, -1, 0),
+        1: (0, 0, 1),
+        2: (-1, 0, 0),
+    },
+    # 4 above
+    {
+        0: (0, 0, -1),
+        1: (0, 1, 0),
+        2: (1, 0, 0),
+    },
+    {
+        0: (0, 0, 1),
+        1: (0, -1, 0),
+        2: (1, 0, 0),
+    },
+    {
+        0: (0, 1, 0),
+        1: (0, 0, 1),
+        2: (1, 0, 0),
+    },
+    {
+        0: (0, -1, 0),
+        1: (0, 0, -1),
+        2: (1, 0, 0),
+    },
+]
+
 
 def multiply(matrix, vector):
     return (
@@ -26,6 +156,30 @@ def minus_pairs(vector1, vector2):
     )
 
 
+def map_scanner(unknown_scanner, beacons_real_coordinates, scanners, scanner_coords):
+    print(f'Trying to map scanner {unknown_scanner}')
+    for point in copy(beacons_real_coordinates):
+        for point_2 in scanners[unknown_scanner]:
+            for rotation_matrix in ROTATION_MATRIXES:
+                # Let's assume that point and point_2 are the same beacon and then test our assumption
+                scanner_2_coord = minus_pairs(point, multiply(rotation_matrix, point_2))
+                matching_points = 0
+                for test_point in scanners[unknown_scanner]:
+                    test_coord_in_s1 = sum_pairs(multiply(rotation_matrix, test_point), scanner_2_coord)
+                    if test_coord_in_s1 in beacons_real_coordinates:
+                        matching_points += 1
+                        if matching_points >= 12:
+                            # That's a good match, let's add all beacons
+                            print(f'Found match for scanner {unknown_scanner}')
+                            for good_point in scanners[unknown_scanner]:
+                                beacons_real_coordinates.add(sum_pairs(multiply(rotation_matrix, good_point),
+                                                                       scanner_2_coord))
+
+                            scanner_coords[unknown_scanner] = scanner_2_coord
+                            return True
+    return False
+
+
 if __name__ == '__main__':
     scanners = {}
     with open(os.path.join('..', 'day_19_input.txt'), 'r') as f:
@@ -39,174 +193,21 @@ if __name__ == '__main__':
                 point = tuple(map(int, line.split(',')))
                 scanners[current_scanner].add(point)
 
-    # http://www.euclideanspace.com/maths/algebra/matrix/transforms/examples/index.htm
-    rotation_matrixes = [
-        {
-            0: (1, 0, 0),
-            1: (0, 1, 0),
-            2: (0, 0, 1),
-        },
-        {
-            0: (1, 0, 0),
-            1: (0, 0, -1),
-            2: (0, 1, 0),
-        },
-        {
-            0: (1, 0, 0),
-            1: (0, -1, 0),
-            2: (0, 0, -1),
-        },
-        {
-            0: (1, 0, 0),
-            1: (0, 0, 1),
-            2: (0, -1, 0),
-        },
-        # 4 above
-
-        {
-            0: (0, -1, 0),
-            1: (1, 0, 0),
-            2: (0, 0, 1),
-        },
-        {
-            0: (0, 0, 1),
-            1: (1, 0, 0),
-            2: (0, 1, 0),
-        },
-        {
-            0: (0, 1, 0),
-            1: (1, 0, 0),
-            2: (0, 0, -1),
-        },
-        {
-            0: (0, 0, -1),
-            1: (1, 0, 0),
-            2: (0, -1, 0),
-        },
-        # 4 above
-        {
-            0: (-1, 0, 0),
-            1: (0, -1, 0),
-            2: (0, 0, 1),
-        },
-        {
-            0: (-1, 0, 0),
-            1: (0, 0, -1),
-            2: (0, -1, 0),
-        },
-        {
-            0: (-1, 0, 0),
-            1: (0, 1, 0),
-            2: (0, 0, -1),
-        },
-        {
-            0: (-1, 0, 0),
-            1: (0, 0, 1),
-            2: (0, 1, 0),
-        },
-        # 4 above
-        {
-            0: (0, 1, 0),
-            1: (-1, 0, 0),
-            2: (0, 0, 1),
-        },
-        {
-            0: (0, 0, 1),
-            1: (-1, 0, 0),
-            2: (0, -1, 0),
-        },
-        {
-            0: (0, -1, 0),
-            1: (-1, 0, 0),
-            2: (0, 0, -1),
-        },
-        {
-            0: (0, 0, -1),
-            1: (-1, 0, 0),
-            2: (0, 1, 0),
-        },
-        # 4 above
-        {
-            0: (0, 0, -1),
-            1: (0, -1, 0),
-            2: (-1, 0, 0),
-        },
-        {
-            0: (0, 0, 1),
-            1: (0, 1, 0),
-            2: (-1, 0, 0),
-        },
-        {
-            0: (0, 1, 0),
-            1: (0, 0, -1),
-            2: (-1, 0, 0),
-        },
-        {
-            0: (0, -1, 0),
-            1: (0, 0, 1),
-            2: (-1, 0, 0),
-        },
-        # 4 above
-        {
-            0: (0, 0, -1),
-            1: (0, 1, 0),
-            2: (1, 0, 0),
-        },
-        {
-            0: (0, 0, 1),
-            1: (0, -1, 0),
-            2: (1, 0, 0),
-        },
-        {
-            0: (0, 1, 0),
-            1: (0, 0, 1),
-            2: (1, 0, 0),
-        },
-        {
-            0: (0, -1, 0),
-            1: (0, 0, -1),
-            2: (1, 0, 0),
-        },
-    ]
-    # scanner_coords = {0: ((0, 0, 0), rotation_matrixes[0], 0)}
     scanner_coords = {0: (0, 0, 0)}
 
     beacons_real_coordinates = set(scanners[0])
     unknown_scanners = set(scanners.keys())
     unknown_scanners.remove(0)
 
+    # XXX: Just pre-known order of scanners to map to speedup this stuff
+    unknown_scanners = [7, 2, 1, 8, 3, 9, 14, 11, 15, 12, 16, 17, 20, 19, 4, 18, 6, 10, 21, 5, 22, 24, 25, 13, 23]
+
     while unknown_scanners:
-        for unknown_scanner in copy(unknown_scanners):
-            if unknown_scanner not in unknown_scanners:
-                break
-            # Try to map unknown scanner to the block with 0th scanner
-
-            for point in copy(beacons_real_coordinates):
-                if unknown_scanner not in unknown_scanners:
-                    break
-                for point_2 in scanners[unknown_scanner]:
-                    if unknown_scanner not in unknown_scanners:
-                        break
-                    for rotation_matrix in rotation_matrixes:
-                        if unknown_scanner not in unknown_scanners:
-                            break
-                        # Let's assume that point and point_2 are the same beacon and then test our assumption
-                        scanner_2_coord = minus_pairs(point, multiply(rotation_matrix, point_2))
-                        matching_points = 0
-                        for test_point in scanners[unknown_scanner]:
-                            test_coord_in_s1 = sum_pairs(multiply(rotation_matrix, test_point), scanner_2_coord)
-                            if test_coord_in_s1 in beacons_real_coordinates:
-                                matching_points += 1
-                                if matching_points >= 12:
-                                    # That's a good match, let's add all beacons
-                                    print(f'Found match for scanner {unknown_scanner}')
-                                    for good_point in scanners[unknown_scanner]:
-                                        beacons_real_coordinates.add(sum_pairs(multiply(rotation_matrix, good_point),
-                                                                               scanner_2_coord))
-
-                                    unknown_scanners.remove(unknown_scanner)
-                                    scanner_coords[unknown_scanner] = scanner_2_coord
-                                    break
+        still_unknown = []
+        for unknown_scanner in unknown_scanners:
+            if not map_scanner(unknown_scanner, beacons_real_coordinates, scanners, scanner_coords):
+                still_unknown.append(unknown_scanner)
+        unknown_scanners = still_unknown
 
     max_distance = 0
     for scanner1 in scanner_coords:
