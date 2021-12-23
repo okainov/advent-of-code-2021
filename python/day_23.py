e26ef98a238b4b1faa06bb035a3e46f33d1d0a36
Oleg Kainov <kainov.oleg@gmail.com>
2021-12-23 08:15:54 +0100

    day 23: "add" day 23

---
+++ python/day_23.py
@@ -0,0 +1,5 @@
+# No code actually :)
+
+# P1 with pen and paper, had some miscalculations, but it was okay to get 15516
+
+# For P2 was first always getting 45334 which was "too high", but then tried different approach with got me 45272
